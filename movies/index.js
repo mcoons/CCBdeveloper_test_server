a59bb3563bda31637d6202aab1a4fb031593bcaf
mcoons <mcoons67@gmail.com>
2018-10-08 16:51:40 -0600

    added database connectivity and basic list all route

--- movies/index.js
+++ movies/index.js
@@ -1,5 +1,7 @@
 // require/import the express module(s)
 const express = require('express'); 
+const queries = require("./database/queries");
+
 
 // instantiate an instance of the express class as 'app'
 const app = express();              
@@ -8,7 +10,13 @@ const app = express();
 const localport = 3030;
 
 // create a base route to direct root GET requests to
-app.get('/', (request, response, next) => response.send('Hello There'));      
+app.get('/', (request, response, next) => response.send('Hello There'));   
+
+app.get("/movies/", (request, response, next) => {
+    queries
+        .list()
+        .then(movies => { response.json({ movies }); })
+});
 
 // tell the Express app to listen for requests on our port      
 app.listen(localport, () => console.log(`Server is now listening on port ${localport}`));
